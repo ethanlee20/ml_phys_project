cc619aacdec80d2f56963b663d0550566350cd01
ethanlee20 <elee20@hawaii.edu>
2026-01-21 11:39:08 -1000

    no det in sim

--- scripts/kekcc/steer_sim.py
+++ scripts/kekcc/steer_sim.py
@@ -29,24 +29,30 @@ bg_local = glob.glob('/group/belle2/dataprod/BGOverlay/early_phase3/release-06-0
 # set database conditions (in addition to default)
 b2.conditions.prepend_globaltag("mc_production_MC15ri_a")
 
-# create path
+print("Create path")
 main = b2.Path()
 
 # default to early phase 3 (exp=1003), run 0
+print("Add EventInfoSetter")
 main.add_module("EventInfoSetter", expList=1003, runList=0, evtNumList=n_events)
 
 # generate events from decfile
+print("Add EvtGenInput")
 main.add_module('EvtGenInput', userDECFile=b2.find_file(path_file_dec))
 
 # detector simulation
-si.add_simulation(path=main, bkgfiles=bg)
+# print("Add simulation")
+# si.add_simulation(path=main, bkgfiles=bg)
 
 # reconstruction
-re.add_reconstruction(path=main)
+# print("Add reconstruction")
+# re.add_reconstruction(path=main)
 
 # Finally add mdst output (file name overwritten on the grid)
+print("Add mdst output")
 mdst.add_mdst_output(path=main, filename=str(path_file_out))
 
 # process events and print call statistics
+print("Process")
 b2.process(path=main)
 print(b2.statistics)
